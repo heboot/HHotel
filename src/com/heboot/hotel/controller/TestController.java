4ada8ac3c8fc8d965c317daa5bd3bb6ece4873d2
heboot <heboot@126.com>
2015-05-31 09:16:03 +0800

    test commit

--- src/com/heboot/hotel/controller/TestController.java
+++ src/com/heboot/hotel/controller/TestController.java
@@ -14,7 +14,7 @@ public class TestController implements Controller {
 		// 1、收集参数、验证参数 
 		// 2、绑定参数到命令对象
 		// 3、将命令对象传入业务对象进行业务处理
-		// 4、选择下一个页面
+		// 4、选择下一个页面 test
 		ModelAndView mv = new ModelAndView();
 		// 添加模型数据 可以是任意的POJO对象
 		mv.addObject("message", "Hello World!");
